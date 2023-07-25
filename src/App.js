bc611d7137dbf83c71f4f29d43423b1e06a5fd91
Neeraj <nkheria@gmail.com>
2023-07-25 18:47:37 +0530

    first draft of app file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to React App</h1>
+      <hr>
+      </hr>
+      <h3> July 25, 2023 </h3>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default App;
